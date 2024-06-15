37c4ff49d35d2fc5c084d66ac63330982acc0f2e
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-06-15 17:17:04 +0300

    project init

---
+++ src/main/java/net/protsenko/App.java
@@ -0,0 +1,13 @@
+package net.protsenko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
