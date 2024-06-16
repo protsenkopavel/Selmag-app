3340154b2655d810c8325901474480a0f7c72c10
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-06-16 18:59:14 +0300

    catalogue: controllers and handlers done

--- catalogue-service/src/main/java/net/protsenko/catalogue/controller/ProductsRestController.java
+++ catalogue-service/src/main/java/net/protsenko/catalogue/controller/ProductsRestController.java
@@ -5,17 +5,13 @@ import lombok.RequiredArgsConstructor;
 import net.protsenko.catalogue.controller.payload.NewProductPayload;
 import net.protsenko.catalogue.entity.Product;
 import net.protsenko.catalogue.service.ProductService;
-import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 @RestController
@@ -25,8 +21,6 @@ public class ProductsRestController {
 
     private final ProductService productService;
 
-    private final MessageSource messageSource;
-
     @GetMapping
     public List<Product> getProducts() {
         return this.productService.findAllProducts();
@@ -34,22 +28,15 @@ public class ProductsRestController {
 
     @PostMapping
     public ResponseEntity<?> createProduct(@Valid @RequestBody NewProductPayload payload,
-                                                 BindingResult bindingResult,
-                                                 UriComponentsBuilder uriBuilder,
-                                                 Locale locale) {
+                                           BindingResult bindingResult,
+                                           UriComponentsBuilder uriBuilder)
+            throws BindException {
         if (bindingResult.hasErrors()) {
-            ProblemDetail problemDetail = ProblemDetail
-                    .forStatusAndDetail(HttpStatus.BAD_REQUEST,
-                            this.messageSource.getMessage("errors.400.title", new Object[0],
-                                    "errors.400.title", locale));
-            problemDetail.setProperty("errors",
-                    bindingResult.getAllErrors()
-                    .stream()
-                    .map(ObjectError::getDefaultMessage)
-                    .toList());
-
-            return ResponseEntity.badRequest()
-                    .body(problemDetail);
+            if (bindingResult instanceof BindException exception) {
+                throw exception;
+            } else {
+                throw new BindException(bindingResult);
+            }
         } else {
             Product product = this.productService.createProduct(payload.title(), payload.details());
             return ResponseEntity
